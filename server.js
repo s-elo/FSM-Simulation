906d4c2902fdc092e7c47e16d87e7d099aba1261
SuperLi <zz321yy@qq.com>
2021-04-14 22:08:50 +0800

    run at server (node)

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+// const temp = require('art-template');
+const path = require("path");
+
+const server = express();
+
+const port = 5500;
+
+server.use("/public/", express.static(path.join(__dirname, "./public/")));
+server.engine("html", require("express-art-template"));
+
+server.set("views", path.join(__dirname, "./views/"));
+
+server.get("/", (_, res) => {
+  res.render("design.html");
+});
+
+server.get("/design.html", (_, res) => {
+  res.render("design.html");
+});
+
+server.get("/Instruction.html", (_, res) => {
+  res.render("Instruction.html");
+});
+
+server.get("/simulator.html", (_, res) => {
+  res.render("simulator.html");
+});
+
+server.listen(port, function () {
+  console.log(`running at ${port}.....`);
+});
